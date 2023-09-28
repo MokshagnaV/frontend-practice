11ae47773100906cb9cd8e65e471f05da8c6e898
MokshagnaV <mokshagnavemula@gmail.com>
2023-09-28 16:52:45 +0530

    complete header 50%

---
+++ bonsai-pricing-page-clone/src/data.js
@@ -0,0 +1,127 @@
+const productData = [
+  {
+    title: "Bonsai Workflow",
+    description:
+      "Look professional, win more clients and manage your business from one place",
+    children: [
+      {
+        title: "Invoices",
+        description:
+          "Get paid faster with automatic invoicing, online payments and more",
+      },
+      {
+        title: "Proposals",
+        description:
+          "Create client-winning proposals with open receipts, custom packages and electronic approvals",
+      },
+      {
+        title: "Contracts",
+        description:
+          "Protect your business with fully-customizable contract templates. Edit and e-sign hassle free.",
+      },
+      {
+        title: "Client CRM",
+        description:
+          "Add leads, manage existing clients and track projects easily from one place",
+      },
+      {
+        title: "Forms & Automations",
+        description:
+          "Boost client intake and automate feedback with custom web forms and questionnaires",
+      },
+      {
+        title: "Time Tracking",
+        description:
+          "Stay organized with Bonsai's all-in-one time tracker, automated timesheets and billing system",
+      },
+      {
+        title: "Accounting",
+        description:
+          "Manage your finances easily with income reports, expense tracking, tax reminders and more",
+      },
+      {
+        title: "Task Tracking",
+        description:
+          "Stress-free task management with project templates and integrated timesheets",
+      },
+    ],
+  },
+  {
+    title: "Bonsai Tax",
+    description:
+      "Track expenses, maximize tax write-offs, and estimate taxes without the headache",
+  },
+  {
+    title: "Bonsai Cash",
+    description:
+      "Bonsai's all-in-one financial hub: No fees and lightning fast payouts",
+  },
+];
+
+const templatesData = [
+  {
+    title: "Contract Templates",
+    samples: [
+      "Free Contract Maker",
+      "Social Media Management Contract Template",
+      "Graphic Design Contract Template",
+      "Digital Marketing Contract Template",
+    ],
+  },
+  {
+    title: "Proposal Templates",
+    samples: [
+      "Graphic Design Proposal Template",
+      "Business Proposal Template",
+      "Brand Ambassador Proposal Template",
+      "Data Entry Proposal Letter Template",
+    ],
+  },
+  {
+    title: "Invoice Templates",
+    samples: [
+      "Social Media Invoice Templates",
+      "Contractor Invoice Template",
+      "Web Design Invoice Template",
+      "Influencer Invoice Template",
+    ],
+  },
+  {
+    title: "Agreement Templates",
+    samples: [
+      "Influencer Agreement Template",
+      "Influencer Collaboration Agreement Template",
+      "Monthly Retainer Agreement Template",
+      "Writer Agreement Template",
+    ],
+  },
+  {
+    title: "Quote Templates",
+    samples: [
+      "Web Design Quotation Template",
+      "Interior Design Quotation Template",
+      "Digital Marketing Quotation Template",
+      "Video Production Quotation Template",
+    ],
+  },
+  {
+    title: "Scope of Work Templates",
+    samples: [
+      "Website Development Scope of Work Template",
+      "Digital Marketing Scope of Work Template",
+      "Consultant Scope of Work Template",
+      "Interior Design Scope ofWork Template",
+    ],
+  },
+  {
+    title: "Brief Templates",
+    samples: [
+      "Design Brief Template",
+      "Architecture Design Brief",
+      "Fashion Design Brief",
+      "Creative Brief Template",
+    ],
+  },
+];
+
+export { productData, templatesData };
---
+++ bonsai-pricing-page-clone/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        green: "#00b289",
+        grey: "#4c4d5f",
+        white: "#fff",
+        yellow: "#f8db62",
+      },
+    },
+  },
+  plugins: [],
+};
