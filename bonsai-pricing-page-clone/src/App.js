053097a5a1bc8cba44190cc9758cd281b841972c
MokshagnaV <mokshagnavemula@gmail.com>
2023-10-01 10:35:38 +0530

    fix Header overflow on mobile screens

--- bonsai-pricing-page-clone/src/App.js
+++ bonsai-pricing-page-clone/src/App.js
@@ -5,7 +5,7 @@ import Footer from "./components/pages/Footer";
 
 export default function App() {
   return (
-    <div>
+    <div className="overflow-x-hidden">
       <Header />
       <Main />
       <Footer />
